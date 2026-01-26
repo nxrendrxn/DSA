acf52d6ce171151ac4180a68a54cef0be4b57407
Narendran <narendransivaramman@gmail.com>
2026-01-26 22:14:01 +0530

    Time: 79 ms (58.75%) | Memory: 21.8 MB (25.75%) - LeetSync

---
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minimumAbsDifference(self, arr):
+        arr.sort()
+        min_diff = float('inf')
+
+        for i in range(1, len(arr)):
+            diff = arr[i] - arr[i - 1]
+            if diff < min_diff:
+                min_diff = diff
+                
+        result = []
+
+        for i in range(1, len(arr)):
+            if arr[i] - arr[i - 1] == min_diff:
+                result.append([arr[i - 1], arr[i]])
+        
+        return result
+
+        
